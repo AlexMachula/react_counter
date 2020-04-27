3a1df3ab3455328bdc111a489967405883d172bf
Alex Machula <alexmachulamail@gmail.com>
2020-04-27 15:20:13 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.81776d4b4f5e0fd47d9ab9381afd4ce7.js"
 );
 
 self.addEventListener('message', (event) => {
